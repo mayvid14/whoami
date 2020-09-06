385253457f3cf53202a1765d2d9360de5f8a4f2a
mayvid14 <ndivyam14@gmail.com>
2020-09-06 10:55:46 +0000

    Deploying to gh-pages from  @ 7a0e31264c7d64da64e1115312329f50c234c1fd 🚀

---
+++ _next/static/4E0ty82ug2s6VaFKiz2Gk/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F296764e4759e456d0ab2.css","static\u002Fchunks\u002Fpages\u002Findex-a9c6ebf48f9c3cd43724.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-ed1b06dc12d6382f8fbd.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
